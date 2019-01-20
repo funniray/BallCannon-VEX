fdefae3a23457ba02f7773ce5799a301a414df51
funniray <funniray10@gmail.com>
2019-01-20 16:44:21 -0500

    Initial Commit

---
+++ src/opcontrol.c
@@ -0,0 +1,54 @@
+/** @file opcontrol.c
+ * @brief File for operator control code
+ *
+ * This file should contain the user operatorControl() function and any functions related to it.
+ *
+ * PROS contains FreeRTOS (http://www.freertos.org) whose source code may be
+ * obtained from http://sourceforge.net/projects/freertos/files/ or on request.
+ */
+
+#include "main.h"
+#define MULT 1
+
+/*
+ * Runs the user operator control code. This function will be started in its own task with the
+ * default priority and stack size whenever the robot is enabled via the Field Management System
+ * or the VEX Competition Switch in the operator control mode. If the robot is disabled or
+ * communications is lost, the operator control task will be stopped by the kernel. Re-enabling
+ * the robot will restart the task, not resume it from where it left off.
+ *
+ * If no VEX Competition Switch or Field Management system is plugged in, the VEX Cortex will
+ * run the operator control task. Be warned that this will also occur if the VEX Cortex is
+ * tethered directly to a computer via the USB A to A cable without any VEX Joystick attached.
+ *
+ * Code running in this task can take almost any action, as the VEX Joystick is available and
+ * the scheduler is operational. However, proper use of delay() or taskDelayUntil() is highly
+ * recommended to give other tasks (including system tasks such as updating LCDs) time to run.
+ *
+ * This task should never exit; it should end with some kind of infinite loop, even if empty.
+ */
+void operatorControl() {
+	usartInit(uart1, 9600, 0);
+	fputc('s',uart1);
+	double curPos = 0;
+	while (1) {
+		char type;
+		fgetc(uart1);
+		switch (type) {
+			case 'p': ;
+				char buffer[256];
+				fgets(buffer, 256, uart1);
+				curPos = atof(buffer)*MULT;
+				motorSet(1,curPos);
+				break;
+			case 's':
+				fprintf(uart1, "%s", curPos);
+				break;
+			default: ;
+				char b[256];
+				fgets(b, 256, uart1);
+				printf("%s", b);
+		}
+		delay(1);
+	}
+}
